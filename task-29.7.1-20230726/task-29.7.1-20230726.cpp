f471f55a5fb55899827e853ec7d5c41e4b7b0ab6
7taras <7taras@mail.ru>
2023-07-28 08:38:00 +0300

    ...

--- task-29.7.1-20230726/task-29.7.1-20230726.cpp
+++ task-29.7.1-20230726/task-29.7.1-20230726.cpp
@@ -15,7 +15,45 @@ class FineGrainedQueue
 {
     Node* head;
     std::mutex* queue_mutex;
+
+public:
+    void insertIntoMiddle(int value, int pos)
+    {
+        queue_mutex->lock();
+
+        Node* prev = this->head;
+        Node* cur = this->head->next;
+
+        prev->node_mutex->lock();
+        queue_mutex->unlock();
+
+        if (cur != nullptr)
+        {
+            cur->node_mutex->lock();
+        }
+        while (cur != nullptr)
+        {
+            for (int i = 0; i < pos; ++i)
+            {
+                Node* old_prev = prev;
+                prev = cur;
+                cur = cur->next;
+                old_prev->node_mutex->unlock();
+            }
+            if (cur != nullptr)
+            {
+                cur->node_mutex->lock();
+            }
+        }
+        Node* node = new Node;
+        node->value = value;
+        node->next = cur;
+        prev->next = node;
+        prev->node_mutex->unlock();
+        cur->node_mutex->unlock();
+    }
 };
+
 
 int main()
 {
